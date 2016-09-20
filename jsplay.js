219e621f25d8d15964f7aba54896ec2414de53c2
Pedro Mendes <pedrolopesme@gmail.com>
2016-09-19 23:45:18 -0300

    Basic helper functions to manipulate the tree.

--- jsplay.js
+++ jsplay.js
@@ -2,13 +2,90 @@
  * 
  * JSplay
  * 
- * A Javascript implementation of Splay Tree binary search algorithm. 
+ * A Javascript implementation of Splay Tree binary search algorithm.
+ * Supports insert, search and delete. Splays on every operation, regardless
+ * of the presence of the associated key prior to that operation. 
  *
  */
-module.exports = {
+module.exports = function() {
 
-    splay: function( arrayObj ){
-        return arrayObj;
+    var root = null;
+
+    /**
+     * Node definition used throughout the code.
+     */
+    var NodeDefinition = function(key, value){        
+        this.key = key;
+        this.value = value;
+        this.leftNode = null;   // left subtree  
+        this.rightNode = null;  // right subtree
+        this.height = 0;        // node height
+    }
+
+    /**
+     * Splay tree insertion.
+     * 
+     * Adds a node to the tree and Splay it to root node. 
+     */
+    this.add = function(key, value){
+        throw new Error("not implemented yet...");
+    }
+
+    /**
+     * Splay tree main function.
+     * 
+     * Splay a node to the root of the tree. If there isn't a node with 
+     * that key, the last node along the search path for the key is splay to 
+     * the root.   
+     */
+    this.splay = function(node, key){
+        throw new Error("not implemented yet...");
+    }
+
+    /**
+     * Size 
+     * 
+     * Helper function that calculates tree size.
+     */
+    this.size  = function(){
+        return this.treeSize(root);
+    }
+
+    /**
+     * Subtree Size 
+     * 
+     * Helper function that calculates subtree size starting
+     * from a given node;
+     */
+    this.treeSize  = function(node){
+        if(node == null) {
+            return 0;
+        }
+        return 1 + this.treeSize(node.leftNode) + this.treeSize(node.rightNode);
+    }
+
+    /**
+     * Rotate right 
+     * 
+     * Helper function that rotates node positions in closewise direction 
+     */
+    this.rotateRight  = function(node){
+        var left = node.left;
+        node.left  = left.right;
+        node.right = node
+        return left;
+    }
+
+    /**
+     * Rotate left 
+     * 
+     * Helper function that rotates node positions in counterclosewise direction 
+     */
+    this.rotateRight  = function(node){
+        var left = node.right;
+        node.right  = left.left;
+        node.left = node;
+        return left;
     }
 
 }
--- spec/jsplay_spec.js
+++ spec/jsplay_spec.js
@@ -1,11 +1,61 @@
-var jsplay = require("../jsplay");
+var JSplay = require("../jsplay");
 
 describe("JSplay", function(){
 
+    var tree;
+
+    beforeEach(function(){
+        tree = new JSplay();
+    });
+
+    describe("tree", function(){
+        it("should calculate tree size even with no nodes",function(){
+            expect(tree.size()).toEqual(0);
+        });
+
+        xit("should calculate tree size with one node", function(){
+            tree.add(1, "one");
+            expect(tree.size()).toEqual(1);
+        });
+
+        xit("should calculate tree size with tree nodes", function(){
+            tree.add(1, "one");
+            tree.add(2, "two");
+            tree.add(3, "tree");
+            expect(tree.size()).toEqual(3);
+        });
+
+        xit("should calculate tree size with a hundred nodes", function(){
+            for(var i = 1; i <= 100; i++){
+                tree.add(i, "node #" + i);
+            }
+            expect(tree.size()).toEqual(100);
+        });
+
+        xit("should calculate a node height on a tree with no nodes",function(){
+            tree.add(1, "one");
+            var node = tree.get(1);
+            expect(node.heigth).toEqual(0);
+        });
+
+        xit("should splay the last node to the root and update its height",function(){
+            tree.add(3, "tree");
+            tree.add(2, "two");
+            tree.add(1, "one");
+            var node = tree.get(3);
+            expect(node.heigth).toEqual(0); 
+        });
+
+        xit("should rotate nodes to right",function(){
+        });
+
+        xit("should rotate nodes to left",function(){ 
+        });
+    });
+
     describe("sorting", function(){
-        it("should run on empty array", function(){
-            expect(jsplay.splay([])).toEqual([]);
-        })
+        xit("should run on an empty tree", function(){
+        });
     });
     
 });
